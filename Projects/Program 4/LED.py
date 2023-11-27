408ec2b54ccbb63b8ebdf5cb3e301320ce9e8d18
Brady Beecham <45153206+Brady-Beecham@users.noreply.github.com>
2023-11-27 10:28:23 -0600

    Blink period and number of times to blink added
    This has not been tested as I have not had a monitor for the PI. This code will be verified tonight when I have access to a monitor.

--- Projects/Program 4/LED.py
+++ Projects/Program 4/LED.py
@@ -6,9 +6,11 @@ LED_pin = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_pin, GPIO.OUT)
 
-def BlinkLed():
-  GPIO.output(LED_pin, GPIO.HIGH)
-  time.sleep(0.5)
-  GPIO.output(LED_pin,GPIO.LOW)
-  time.sleep(0.5)
+#Blink the LED in a 50% duty cycle every Period seconds NumBlinks times
+def BlinkLed(Period, NumBlinks):
+  for Blink in range(NumBlinks):
+    GPIO.output(LED_pin, GPIO.HIGH)
+    time.sleep(Period/2)
+    GPIO.output(LED_pin,GPIO.LOW)
+    time.sleep(Period/2)
   return
